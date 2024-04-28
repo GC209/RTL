2eab0fc8cb099b955a9d963a54f476111ab1e5bf
Gaurav Chaturvedi <gaurav.c209@gmail.com>
2024-04-28 22:43:08 +0530

    getBy...queries

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -1,12 +1,45 @@
-import { render, screen } from "@testing-library/react";
+import { getByRole, render, screen } from "@testing-library/react";
 import { Application } from "./application";
 
 describe('Application', () => {
   test('reners correctly', () => {
     render(<Application />);
     
-    const nameElement = screen.getByRole('textbox');
+    const jobHeadingElement = screen.getByRole('heading', {
+      level: 1
+    });
+    expect(jobHeadingElement).toBeInTheDocument();
+
+    const sectionElement = screen.getByRole('heading', {
+      level: 2
+    });
+    expect(sectionElement).toBeInTheDocument();
+
+    const nameElement = screen.getByRole('textbox', {
+      name: "Name"
+    });
     expect(nameElement).toBeInTheDocument();
+
+    //alternate way of querying the serch by label text
+    const nameElement2 = screen.getByLabelText('Name');
+    expect(nameElement2).toBeInTheDocument();
+
+    //getByPlaceholderText
+    const nameElement3 = screen.getByPlaceholderText('Fullname');
+    expect(nameElement3).toBeInTheDocument();
+
+    //getByDisplayValue
+    const nameElement4 = screen.getByDisplayValue('Gaurav');
+    expect(nameElement4).toBeInTheDocument();
+
+    const customElement = screen.getByTestId('custom-element');
+    expect(customElement).toBeInTheDocument();
+    
+
+    const bioElement = screen.getByRole('textbox', {
+      name: "Bio"
+    });
+    expect(bioElement).toBeInTheDocument();
 
     const selectElement = screen.getByRole('combobox');
     expect(selectElement).toBeInTheDocument();
--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -1,27 +1,36 @@
 
 export const Application = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
-      </div>
-      <div>
-        <label htmlFor="job-location">Job location</label>
-        <select id="job-location">
-          <option value=''>Select a country</option>
-          <option value='US'>United States</option>
-          <option value='UK'>United Kingdom</option>
-          <option value='IN'>India</option>
-          <option value='AU'>Australia</option>
-        </select>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" id="terms" /> Iagree to the terms and conditions
-        </label>
-      </div>
-      <button>Submit</button>
-    </form>
+    <>
+      <h1>Job application form</h1>
+      <h2>Section 1</h2>
+      <div data-testid="custom-element">Custom Element</div>
+      <form>
+        <div>
+          <label htmlFor="name" >Name</label>
+          <input type="text" id="name" placeholder="Fullname" value="Gaurav"/>
+        </div>
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <input id="bio" name="bio"/>
+        </div>
+        <div>
+          <label htmlFor="job-location">Job location</label>
+          <select id="job-location">
+            <option value=''>Select a country</option>
+            <option value='US'>United States</option>
+            <option value='UK'>United Kingdom</option>
+            <option value='IN'>India</option>
+            <option value='AU'>Australia</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" id="terms" /> Iagree to the terms and conditions
+          </label>
+        </div>
+        <button>Submit</button>
+      </form>
+    </>
   )
 }
