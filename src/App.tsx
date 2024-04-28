2a5dfa976f3dff59333c11b8d4b105bb437ab1fa
Gaurav Chaturvedi <gaurav.c209@gmail.com>
2024-04-28 23:29:19 +0530

    Query multiple elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Greet } from "./components/greet/greet";
-import { Application } from "./components/application/application";
+// import { Greet } from "./components/greet/greet";
+// import { Application } from "./components/application/application";
+import Skills from "./components/skills/skills";
 function App() {
   return (
     <div className="App">
-      <Greet name="Gaurav" />
-      <Application />
+      {/* <Greet name="Gaurav" />
+      <Application /> */}
+      <Skills skills={['HTML', 'CSS']} />
     </div>
   );
 }
