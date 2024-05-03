b570d84a17d6b878ae01be68a938d4173ee9a786
Gaurav Chaturvedi <gaurav.c209@gmail.com>
2024-05-04 01:20:45 +0530

    custom hooks implementation using act utility

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@
 // import { Application } from "./components/application/application";
 //import Skills from "./components/skills/skills";
 //import Counter from "./components/counter/counter";
-import { Appproviders } from "./providers/app-providers";
-import { MuiMode } from "./components/mui/mui-mode";
+// import { Appproviders } from "./providers/app-providers";
+// import { MuiMode } from "./components/mui/mui-mode";
 function App() {
   return (
-    <Appproviders>
-      <div className="app">
-        <MuiMode />
-      </div>
-    </Appproviders>
+    // <Appproviders>
+    //   <div className="app">
+    //     <MuiMode />
+    //   </div>
+    // </Appproviders>
+    <></>
     // <div className="App">
     //   {/* <Greet name="Gaurav" />
     //   <Application />
